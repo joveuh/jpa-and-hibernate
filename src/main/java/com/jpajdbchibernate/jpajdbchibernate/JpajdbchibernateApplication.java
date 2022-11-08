06f3e9077b466e85f04f30a3764725f0f93f5871
Uzair H <uzair@ibm.com>
2022-11-08 10:16:04 -0500

    setting up

---
+++ src/main/java/com/jpajdbchibernate/jpajdbchibernate/JpajdbchibernateApplication.java
@@ -0,0 +1,13 @@
+package com.jpajdbchibernate.jpajdbchibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpajdbchibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpajdbchibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpajdbchibernate/jpajdbchibernate/JpajdbchibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpajdbchibernate.jpajdbchibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpajdbchibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
